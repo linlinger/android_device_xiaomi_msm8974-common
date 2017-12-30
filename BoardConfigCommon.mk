0e6851b3309440d096eb1f228623351871d446de
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    msm8974-common: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,9 +215,11 @@ TARGET_USES_WCNSS_CTRL           := true
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 -include vendor/xiaomi/msm8974-common/BoardConfigVendor.mk
